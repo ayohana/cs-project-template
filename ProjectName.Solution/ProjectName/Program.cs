996ed42c06977ac3df68b52bdf8b3ae351d6ab82
Adela Darmansyah <52761827+ayohana@users.noreply.github.com>
2020-03-03 10:23:39 -0800

    Create Program.cs file and add boilerplate code

---
+++ ProjectName.Solution/ProjectName/Program.cs
@@ -0,0 +1,16 @@
+// using System;
+// using System.Collections.Generic;
+using ProjectName.Models;
+
+namespace ProjectName
+{
+  public class Program
+  {
+    public static void Main()
+    {
+
+      // UI code goes here
+
+    }
+  }
+}
