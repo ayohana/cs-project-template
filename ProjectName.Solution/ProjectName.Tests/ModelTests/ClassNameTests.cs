ebb357f23186c934e9d59742495d27c6eaeb6437
Adela Darmansyah <52761827+ayohana@users.noreply.github.com>
2020-03-06 09:58:55 -0800

    Update boilerplate in ClassNameTests.cs

--- ProjectName.Solution/ProjectName.Tests/ModelTests/ClassNameTests.cs
+++ ProjectName.Solution/ProjectName.Tests/ModelTests/ClassNameTests.cs
@@ -6,6 +6,11 @@ namespace ProjectName.Tests
   [TestClass]
   public class ClassNameTests
   {
-    // Test methods go here
+    [TestMethod]
+    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    }
   }
 }
